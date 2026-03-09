c4f39e6a0ed741a8101e4aa292252d2f625699ef
Ritesh Parmar <ritesh.parmar@techgrains.com>
2026-03-09 15:17:07 +0530

    Create Data, Reository for connect Database.

---
+++ SurveyManagement.Infrastructure/Data/AppDbContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SurveyManagement.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // DbSet properties for your entities go here
+
+       public DbSet<Survey> Surveys { get; set; }
+    }
+}
---
+++ SurveyManagement.Infrastructure/Repositories/SurveyRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyManagement.Domain.Entities;
+using SurveyManagement.Domain.Interfaces;
+using SurveyManagement.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SurveyManagement.Infrastructure.Repositories
+{
+    public class SurveyRepository : ISurveyRepository
+    {
+        private readonly AppDbContext _context;
+        public SurveyRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Survey survey)
+        {
+            await _context.Surveys.AddAsync(survey);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Survey>> GetAllAsync()
+        {
+            return await _context.Surveys.ToListAsync();
+        }
+
+        public async Task<Survey?> GetByIdAsync(int id)
+        {
+            return await _context.Surveys.FindAsync(id);
+        }
+    }
+}
